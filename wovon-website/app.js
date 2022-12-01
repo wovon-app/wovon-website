957c7626f807cfbe659f909a01805469e63ddeb7
Alejo Flores Lucey <alejo@misterflores.com>
2022-12-01 16:54:47 -0300

    Started connection of auth0

--- wovon-website/app.js
+++ wovon-website/app.js
@@ -8,6 +8,19 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 
+const { auth } = require('express-openid-connect');
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: `${process.env.AUTH0_SECRET}`,
+    baseURL: 'http://localhost:3000',
+    clientID: 'sib80y7mk79Lh3D27fBiOthg15lSTtR3',
+    issuerBaseURL: 'https://dev-k6xh810ejrk8x0ze.us.auth0.com',
+    routes: {
+        callback: '/loggedin'
+    },
+};
+
 dotenv.config();
 
 app.use(logger("dev"));
@@ -16,6 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/", indexRouter);
+app.use(auth(config));
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
 module.exports = app;
--- wovon-website/routes/index.js
+++ wovon-website/routes/index.js
@@ -2,19 +2,24 @@ const express = require("express");
 const router = express.Router();
 const path = require('path');
 
+
+
 const PaymentController = require("../controllers/PaymentController");
 const PaymentService = require("../services/PaymentService");
 
 const PaymentInstance = new PaymentController(new PaymentService());
 
-
 router.get("/", function (req, res, next) {
     return res.sendFile(path.join(__dirname, '../public', 'index.html'));
-})
+});
+
+router.post("/loggedin", function (req, res, next) {
+    res.redirect(req.baseUrl + "/profile");
+});
 
 router.get("/profile", function (req, res, next) {
     return res.sendFile(path.join(__dirname, '../public', 'profile.html'));
-})
+});
 
 router.get("/payment/make/basic", async function (req, res, next) {
     const link = await PaymentInstance.getPaymentLink(req, res, 10, "Wovon API - Plan Básico", req.query.email, req.query.userId, 10, req.query.randomKey);
@@ -34,10 +39,10 @@ router.get("/payment/make/platinium", async function (req, res, next) {
 router.get("/payment/success", function (req, res, next) {
     console.log(__dirname)
     return res.sendFile(path.join(__dirname, '../public', 'success.html'));
-})
+});
 
 router.get("/payment/failure", function (req, res, next) {
     return res.sendFile(path.join(__dirname, '../public', 'failure.html'));
-})
+});
 
 module.exports = router;
